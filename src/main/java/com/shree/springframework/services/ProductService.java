da7985ae736aa78525d4ba794e4aec7492b1726d
srivatssan <srivatssan@hotmail.com>
2016-10-30 19:32:27 -0400

    Created a Product page and Connected it to products page.

--- src/main/java/com/shree/springframework/services/ProductService.java
+++ src/main/java/com/shree/springframework/services/ProductService.java
@@ -9,4 +9,5 @@ import java.util.List;
  */
 public interface ProductService {
     List<Product> listAllProducts();
+    Product getProductById(Integer id);
 }
--- src/main/java/com/shree/springframework/services/ProductServiceImpl.java
+++ src/main/java/com/shree/springframework/services/ProductServiceImpl.java
@@ -5,6 +5,7 @@ import com.sun.xml.internal.ws.developer.Serialization;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -33,13 +34,13 @@ public class ProductServiceImpl implements ProductService {
         product1.setId(1);
         product1.setDescription("Product 1");
         product1.setImageUrl("http://products/images/product1.jpg");
-        product1.setPrice(new BigDecimal(12.99));
+        product1.setPrice(new BigDecimal(12.99).setScale(2, RoundingMode.CEILING));
         products.put(1,product1);
 
         //Creating second Product
         Product product2 = new Product();
         product2.setImageUrl("http://products/images/product2.jpg");
-        product2.setPrice(new BigDecimal(10.32));
+        product2.setPrice(new BigDecimal(10.32).setScale(2, RoundingMode.CEILING));
         product2.setDescription("Product 2");
         product2.setId(2);
         products.put(2,product2);
@@ -48,7 +49,7 @@ public class ProductServiceImpl implements ProductService {
         Product product3 = new Product();
         product3.setId(3);
         product3.setDescription("Product 3");
-        product3.setPrice(new BigDecimal(45.65));
+        product3.setPrice(new BigDecimal(45.65).setScale(2, RoundingMode.CEILING));
         product3.setImageUrl("http://products/images/product3.jpg");
         products.put(3,product3);
 
@@ -57,19 +58,21 @@ public class ProductServiceImpl implements ProductService {
         Product product4 = new Product();
         product4.setImageUrl("http://products/images/product4.jpg");
         product4.setDescription("Product 4") ;
-        product4.setPrice(new BigDecimal(34.23));
+        product4.setPrice(new BigDecimal(34.23).setScale(2, RoundingMode.CEILING));
         product4.setId(4);
         products.put(4,product4);
 
         //Creating the fifth product
         Product product5 = new Product();
         product5.setId(5);
-        product5.setPrice(new BigDecimal(28.94));
+        product5.setPrice(new BigDecimal(28.94).setScale(2, RoundingMode.CEILING));
         product5.setImageUrl("http://products/images/product5.jpg");
         product5.setDescription("Product 5");
         products.put(5,product5);
+    }
 
-
-
+    @Override
+    public Product getProductById(Integer id) {
+        return products.get(id);
     }
 }
